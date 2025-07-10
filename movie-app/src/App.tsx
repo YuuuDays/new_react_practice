937cedc2213546a4f67efb3b8928566a5f761b22
hikida <ponponda0103@gmail.com>
2025-07-10 23:42:52 +0900

    依存配列の勉強会

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState,useEffect } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-// 追加
 type Movie = {
   id: string;
   original_title: string;
@@ -9,7 +8,6 @@ type Movie = {
   overview: string;
 };
 
-// 追加
 type MovieJson = {
   adult: boolean;
   backdrop_path: string | null;
@@ -28,98 +26,55 @@ type MovieJson = {
 };
 
 function App() {
-
-  //Jsのコードを書く
-    const defaultMovieList = [
-    {
-      id: 1,
-      name: "君の名は",
-      image:
-        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/yLglTwyFOUZt5fNKm0PWL1PK5gm.jpg",
-      overview:
-        "1ヵ月後に1000年ぶりの彗星が訪れる日本。東京で暮らす平凡な男子高校生・瀧と、山深い村で都会の生活に憧れながら憂鬱な日々を送る女子高校生・三葉。つながりのない2人は、互いが入れ替わる不思議な夢を見る。",
-    },
-    {
-      id: 2,
-      name: "ハウルの動く城",
-      image:
-        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/v0K2e1t6ocUNnkZ9BeiFdcOT9LG.jpg",
-    },
-    {
-      id: 3,
-      name: "もののけ姫",
-      image:
-        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/mVdz3vlmioKWZaHTGfu99zIuayZ.jpg",
-    },
-    {
-      id: 4,
-      name: "バック・トゥ・ザ・フューチャー",
-      image:
-        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/oHaxzQXWSvIsctZfAYSW0tn54gQ.jpg",
-    },
-  ];
-  //追加
-  const fetchMovieList = async () =>{
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?language=ja&page=1`,
-      {
-        headers:{
-          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-        },
-      }
-    );
+  // 修正
+  const fetchMovieList = async () => {
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+    let url = "";
+    if (keyword) {
+      url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ja&page=1`;
+    } else {
+      url = "https://api.themoviedb.org/3/movie/popular?language=ja&page=1";
+    }
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    });
     const data = await response.json();
-    // useStateに型を入れ込みます
-    // mapとはJavaScriptの配列に対して使えるメソッドです
-    setMovieList(
-      data.results.map((movie: Movie) => ({
-        id: movie.id,
-        original_title: movie.original_title,
-        poster_path: movie.poster_path,
-        overview: movie.overview,
-      }))
-    );
-  
-    console.log('★tyeofdata = ',typeof(data));
-    console.log('★data.resul = ',typeof(data.resuls));
-    console.log(data.results);
+    const result = data.results;
+    const movieList = result.map((movie: MovieJson) => ({
+      id: movie.id,
+      original_title: movie.title,
+      poster_path: movie.poster_path,
+    }));
+    setMovieList(movieList);
+  };
 
-    return data.result;
-  }
+  const [keyword, setKeyword] = useState("");
+  const [movieList, setMovieList] = useState<Movie[]>([]);
 
-  // React(関数コンポーネント)ではuseStateで定義された状態(state)が変化するとコンポーネントが再レンダリングされる
-  const[keyword,setKeyword] = useState("");
-  const[movieList, setMovieList] = useState<Movie[]>([]);
-  
-  //第一引数に実行したい処理を含む関数、第二引数に依存配列を渡す
-  useEffect(() =>{
-    //実行させる処理
+  useEffect(() => {
     fetchMovieList();
-  },[]);
-  
-  // e はイベントオブジェクトで、e.target.value で入力された文字列を取得できる
+  }, [keyword]); // 修正
+
   return (
     <div>
       <div>{keyword}</div>
-      <input type="text" onChange={(e) =>setKeyword(e.target.value)}/>
-
+      <input type="text" onChange={(e) => setKeyword(e.target.value)} />
       {movieList
         .filter((movie) => movie.original_title.includes(keyword))
-        .map((movie) =>(
-        <div key={movie.id}>
-          <h2>{movie.original_title}</h2>
-          <img
+        .map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.original_title}</h2>
+            <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.original_title}
             />
-          <p>{movie.overview}</p>
-          
-          
-        </div>
-      ))}
+            <p>{movie.overview}</p>
+          </div>
+        ))}
     </div>
-     
-  )
+  );
 }
 
-export default App
+export default App;
